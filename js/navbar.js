dccc973b6e1bf2422eefe2dd803d9f20236f764e
rosensh <rosechanelxoxo@gmail.com>
2024-09-01 02:24:31 -0700

    Initial commit of my website

---
+++ js/navbar.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var navbarHTML = `
+        <nav class="navbar">
+            <ul class="nav-links">
+                <li><a href="main.html">Home</a></li>
+                <li><a href="research.html">Research</a></li>
+                <li><a href="teaching.html">Teaching</a></li>
+                <li><a href="docs/RoseNiousha_CV.pdf">CV</a></li>
+            </ul>
+            <div class="hamburger" id="hamburger">
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+            </div>
+        </nav>
+    `;
+
+    // Select the element where you want to place the navbar
+    var navbarPlaceholder = document.getElementById('navbar-placeholder');
+    if (navbarPlaceholder) {
+        navbarPlaceholder.innerHTML = navbarHTML;
+    }
+
+    // JavaScript to toggle the navbar on mobile
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
+});
+
